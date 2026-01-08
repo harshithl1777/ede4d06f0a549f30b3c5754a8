bda6f7ae5a9fc900ba560c4afe37c1deb8ac7e7a
harshithl1777 <harshithl1777@gmail.com>
2026-01-07 19:11:59 -0500

    1767831119

--- main.py
+++ main.py
@@ -4,24 +4,29 @@ import pytz
 import os
 import time
 
+tz = pytz.timezone("America/Toronto")
+
 # Decide whether or not to push on this day (75% push rate)
 should_push_today = random.randrange(1, 13) > 3
 
-log_est_now = pytz.utc.localize(datetime.datetime.now()).astimezone(pytz.timezone("America/Toronto"))
+# Correct timezone handling
+log_est_now = datetime.datetime.now(pytz.UTC).astimezone(tz)
 log_formatted_est_now = log_est_now.strftime("%A, %B %d, %Y - %I:%M:%S %p")
+
 push_status = "[PUSH]" if should_push_today else "[NO PUSH]"
 with open("log.txt", "a") as log:
     log.write(f"\n{push_status} {log_formatted_est_now}")
-log.close()
 
 if should_push_today:
     num_of_contributions = random.randrange(1, 11)
-    for x in range(num_of_contributions):
-        # Generate formatted date and time in EST
+
+    for _ in range(num_of_contributions):
+        est_now = datetime.datetime.now(pytz.UTC).astimezone(tz)
+        formatted_est_now = est_now.strftime("%A, %B %d, %Y - %I:%M:%S %p")
+
         with open("datetimes.txt", "a") as datetimes:
-            est_now = pytz.utc.localize(datetime.datetime.now()).astimezone(pytz.timezone("America/Toronto"))
-            formatted_est_now = est_now.strftime("%A, %B %d, %Y - %I:%M:%S %p")
             datetimes.write(f"\n{formatted_est_now}")
-            os.system(f"git add . && git commit -m {round(time.time())}")
-        datetimes.close()
+
+        os.system(f'git add . && git commit -m "{int(time.time())}"')
+
     os.system("git push origin main")
